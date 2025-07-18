468acc393688170bc678263b56d3fbe726dfac41
Kat Yeary <katyeary@gmail.com>
2025-07-18 14:45:34 -0400

    remove unnecessary client files

---
+++ client/src/lib/utils.ts
@@ -0,0 +1,10 @@
+export function debounce<T extends (...args: any[]) => any>(
+  fn: T,
+  delay: number
+): T {
+  let timeout: ReturnType<typeof setTimeout>;
+  return function (...args: Parameters<T>) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => fn(...args), delay);
+  } as T;
+}
